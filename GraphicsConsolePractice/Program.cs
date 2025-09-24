569ac7eb81fbb4e748ef8332b98771664683f648
jeremias cardoso <jeremiascardoso00@outlook.com>
2025-09-23 23:14:17 -0300

    adding practice app

---
+++ GraphicsConsolePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace GraphicsConsolePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int centerX = Console.WindowWidth / 2;
+            int centerY = Console.WindowHeight / 2;
+
+
+            using (Spinner spinner = new Spinner(centerX, centerY))
+            {
+
+                spinner.Start();
+                Thread.Sleep(10000);
+
+                spinner.Stop();
+                Console.Write("Stop");
+            }
+                
+      
+
+        }
+    }
+}
---
+++ GraphicsConsolePractice/utils/ConsoleSpinner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GraphicsConsolePractice
+{
+    class Spinner : IDisposable
+    {
+
+        private int _posX;
+        private int _posY;
+        private int _delayMs;
+        private bool _isSpinning;
+        private int _index = 0;
+        private Thread _spinnerThread;
+
+
+        public static readonly string[] Basic = { "|", "/", "-", "\\" };
+
+        //Constructor
+        public Spinner (int x, int y, int delayMs = 100)
+        {
+            _posX = x;
+            _posY = y;
+            _delayMs = delayMs;
+            _isSpinning = false;
+        }
+
+        public void Start()
+        {
+            if (_isSpinning) return;
+
+            _isSpinning = true;
+            Console.CursorVisible = false;
+            _spinnerThread = new Thread(SpinnerLoop);
+            _spinnerThread.IsBackground = true;
+            _spinnerThread.Start();
+
+        }
+
+        private void SpinnerLoop()
+        {
+            Console.CursorVisible = false;
+
+            try
+            {
+                while (_isSpinning)
+                {
+                    Console.SetCursorPosition(_posX, _posY);
+                    Console.Write(Basic[_index]);
+                    Thread.Sleep(_delayMs);
+                    _index = (_index + 1) % Basic.Length;
+                }
+            }
+            finally
+            {
+                Console.SetCursorPosition(_posX, _posY);
+                Console.Write(' ');
+                Console.CursorVisible = true;
+            }
+        }
+
+        public void Stop()
+        {
+            _isSpinning = false;
+            if (_spinnerThread.ThreadState == ThreadState.Running)
+            {
+                _spinnerThread.Join(1000);
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
